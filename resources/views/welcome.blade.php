fbc9f065eaabf424664e3d67ea1df6c72bbf0a82
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-02-26 14:28:01 -0500

    Tailwind Config Cont.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        @vite('resources/css/app.css')
 
-        <title>Laravel</title>
+        <title>Lara-Blog</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
