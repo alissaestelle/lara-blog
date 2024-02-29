b416ca26aa4f66fc6383e1a3d5212e1ab3081b0d
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-02-29 11:45:53 -0500

    Blade Implementation >>

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -42,36 +42,35 @@
 
                 <div
                     class="mx-auto mt-10 grid max-w-2xl grid-cols-2 gap-x-10 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    <?php foreach ($posts as $post): ?>
-
-                    <article class="flex max-w-80 flex-col items-start">
-                        <div class="flex items-center gap-x-4 text-xs">
-                            <time datetime="<?= $post->date ?>" class="text-gray-500">
-                                <?= date('F jS, Y', $post->date) ?>
-                            </time>
-                            <a
-                                href="#"
-                                class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-                                <?= $post->tag ?>
-                            </a>
-                        </div>
-                        <div class="group relative">
-                            <h3
-                                class="mt-4 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                                <a href="posts/<?= $post->url ?>">
-                                    <span class="absolute inset-0"></span>
-
-                                    <?= $post->title ?>
+                    @foreach ($posts as $post)
+                    {{-- @dd($loop) --}}
+                        <article class="flex max-w-80 flex-col items-start">
+                            <div class="flex items-center gap-x-4 text-xs">
+                                <time datetime="{{ $post->date }}" class="{{ $loop->first ? 'text-sky-700' : '' }} text-gray-500">
+                                    {{ date('F jS, Y', $post->date) }}
+                                </time>
+                                <a
+                                    href="#"
+                                    class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
+                                    {{ $post->tag }}
                                 </a>
-                            </h3>
+                            </div>
+                            <div class="group relative">
+                                <h3
+                                    class="mt-4 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                    <a href="posts/{{ $post->url }}">
+                                        <span class="absolute inset-0"></span>
 
-                            <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                                <?= $post->excerpt ?>
-                            </p>
-                        </div>
-                    </article>
+                                        {{ $post->title }}
+                                    </a>
+                                </h3>
 
-                    <?php endforeach; ?>
+                                <p class="{{ $loop->first ? 'text-fuchsia-800' : '' }} mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
+                                    {{ $post->excerpt }}
+                                </p>
+                            </div>
+                        </article>
+                    @endforeach
                 </div>
             </div>
         </div>
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -36,27 +36,28 @@
 
                 <div
                     class="mx-auto mt-10 max-w-2xl border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-                    <article class="max-w-[90%]
-                     flex items-center justify-center">
+                    <article class="max-w-[90%] flex items-center justify-center">
                         <div class="flex flex-col items-start">
                             <div class="flex items-center gap-x-4 text-xs">
-                                <time datetime="<?= $post->date ?>" class="text-gray-500">
-                                    <?= date('F jS, Y', $post->date) ?>
+                                <time datetime="{{ $post->date }}" class="text-gray-500">
+                                    {{ date('F jS, Y', $post->date) }}
                                 </time>
                                 <a
                                     href="#"
                                     class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">
-                                    <?= $post->tag ?>
+                                    {{ $post->tag }}
                                 </a>
                             </div>
                             <div class="group relative">
-                                <h3 class="mt-4 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                                <h3
+                                    class="mt-4 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                                     <span class="absolute inset-0"></span>
-                                    <?= $post->title ?>
+                                    {{ $post->title }}
                                 </h3>
                                 <div class="flex items-center gap-8">
                                     <div>
-                                        <?= $post->body ?>
+                                        {{-- Use {!! !!} for Any Content Containing HTML --}}
+                                        {!! $post->body !!}
                                     </div>
                                     <div class="min-w-fit">
                                         <a
