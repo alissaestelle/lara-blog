a51be25be767a00e1042b3a9e2e97f4a0a1e070a
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-03-07 11:04:17 -0500

    Tidy

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        @vite('resources/css/app.css')
 
-        <title>Lara-Blog</title>
-    </head>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible"
+          content="ie=edge" />
+    @vite('resources/css/app.css')
 
-    <body class="px-6 mx-auto max-w-5xl lg:px-8">
-        {{ $nav }}
-        <div class="py-24 bg-white">
-            <div>
-                {{-- {{ $slot }} --}}
-                {{ $header }}
-                {{ $featured }}
-                {{ $main }}
-            </div>
+    <title>Lara-Blog</title>
+</head>
+
+<body class="px-6 mx-auto max-w-5xl lg:px-8">
+    {{ $nav }}
+    <div class="py-24 bg-white">
+        <div>
+            {{-- {{ $slot }} --}}
+            {{ $header }}
+            {{ $featured }}
+            {{ $main }}
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,21 +1,17 @@
-<nav
-    class="pt-6 flex flex-col items-center gap-y-2 xs:px-5 xs:py-6 xs:flex-row xs:justify-between xs:gap-y-0"
->
+<nav class="pt-6 flex flex-col items-center gap-y-2 xs:px-5 xs:py-6 xs:flex-row xs:justify-between xs:gap-y-0">
     <div class="flex-grow">
         <a href="/">
-            <img src="#" alt="Add Logo" />
-            {{-- <img src="#" alt="Add Logo" width="165" height="16" /> --}}
+            <img src="../images/howls.png"
+                 alt="Add Logo"
+                 class="w-16 h-16" />
         </a>
     </div>
 
-    <div
-        class="flex flex-col items-center gap-y-2 xs:flex-row xs:gap-x-4 xs:gap-y-0"
-    >
-        <a href="/" class="text-xs font-bold uppercase">Home Page</a>
-        <a
-            href="#"
-            class="px-4 py-2 flex-grow bg-blue-500 text-xs font-semibold text-white uppercase rounded-full"
-        >
+    <div class="flex flex-col items-center gap-y-2 xs:flex-row xs:gap-x-4 xs:gap-y-0">
+        <a href="/"
+           class="text-xs font-bold uppercase">Home Page</a>
+        <a href="#"
+           class="px-4 py-2 flex-grow bg-blue-500 text-xs font-semibold text-white uppercase rounded-full">
             Subscribe for Updates
         </a>
     </div>
