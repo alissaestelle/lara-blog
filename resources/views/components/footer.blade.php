d1a89e1b4f0f57fc7cb151276794221c97b8779f
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-03-08 11:24:11 -0500

    -_-

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,27 +1,26 @@
-<footer class="mt-6 py-16 px-10 bg-gray-100 border border-black border-opacity-5 rounded-xl text-center">
+<footer class="my-6 py-8 xs:px-10 xs:py-16 bg-gray-100 border border-black border-opacity-5 rounded-xl text-center">
     <img src="../images/howls.png"
          alt="Calcifer"
-         class="mx-auto -mb-2 w-28" />
-    <h5 class="text-3xl">let’s stay in touch</h5>
+         class="mx-auto -mb-2 w-20 xs:w-28" />
+    <h5 class="text-xl text-3xl">let’s stay in touch</h5>
 
     <div class="mt-8">
-        <div class="mx-auto relative inline-block rounded-full md:w-1/2 lg:bg-gray-200 ">
+        <div class="mx-auto w-[80%] xs:w-[60%] relative inline-block rounded-full sm:bg-white lg:bg-gray-200 ">
             <form method="POST"
                   action="#"
-                  class="text-sm md:flex md:justify-between">
-                <div class="flex items-center lg:py-3 lg:px-5">
+                  class="text-sm sm:flex sm:flex-row">
+                <div class="flex-1 flex items-center lg:py-3 lg:px-5">
                     <label for="email"
                            class="hidden lg:inline-block">
                     </label>
-
                     <input id="email"
                            type="text"
                            placeholder="Email Address"
-                           class="pl-4 focus-within:outline-none lg:bg-transparent py-2 lg:py-0" />
+                           class="pl-4 flex-1 rounded-full focus-within:outline-none lg:bg-transparent py-2 lg:py-0" />
                 </div>
 
                 <button type="submit"
-                        class="mt-4 px-8 py-3 transition-colors duration-300 bg-blue-500 rounded-full text-xs font-semibold text-white uppercase lg:mt-0 lg:ml-3 hover:bg-blue-600">
+                        class="mt-3 px-8 py-3 transition-colors duration-300 bg-blue-500 rounded-full text-xs font-semibold text-white uppercase sm:mt-0 lg:ml-3 hover:bg-blue-600">
                     Subscribe
                 </button>
             </form>
