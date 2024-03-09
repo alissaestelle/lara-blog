d8d491a6ac23edd997b624c95a2de36b7752617f
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-03-09 09:33:54 -0500

    Small Nav Edit

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,34 +1,50 @@
 <nav
      class="mx-auto pt-6 flex flex-wrap items-center justify-center gap-x-2 gap-y-2 min-[600px]:justify-between sm:px-5 sm:py-6 sm:flex-nowrap sm:gap-y-0">
-   <div class="hidden min-[600px]:flex min-[600px]:items-center">
+
    <a href="/"
-      class="w-full min-[600px]:w-auto min-[600px]:shrink-0">
+      class="w-full min-[600px]:hidden">
       <img src="../images/howls.png"
            alt="Add Logo"
-           class="mx-auto w-12 h-12 sm:mx-0" />
+           class="mx-auto w-12 h-12" />
    </a>
    <a href="/"
-      class="text-xs font-bold uppercase min-[600px]:pl-1">Resources</a>
+      class="text-xs font-bold uppercase min-[600px]:hidden">Resources</a>
    <a href="/"
-      class="text-xs font-bold uppercase min-[600px]:pl-4">Archive</a>
+      class="text-xs font-bold uppercase min-[600px]:hidden">Archive</a>
    <a href="/"
-      class="text-xs font-bold uppercase min-[600px]:pl-4">About</a>
-</div>
+      class="text-xs font-bold uppercase min-[600px]:hidden">About</a>
+
    <a href="/"
       class="text-xs font-bold uppercase min-[600px]:hidden">Account</a>
-   <div class="w-full text-center sm:w-auto min-[600px]:hidden">
+   <div class="mt-1 w-full text-center min-[600px]:hidden">
       <a href="#"
-         class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full sm:flex-grow sm:w-auto">
+         class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full">
          Subscribe for Updates
       </a>
+   </div>
+
+
+   <div class="hidden min-[600px]:flex min-[600px]:items-center">
+      <a href="/"
+         class="w-full min-[600px]:w-auto min-[600px]:shrink-0">
+         <img src="../images/howls.png"
+              alt="Add Logo"
+              class="mx-auto w-12 h-12 sm:mx-0" />
+      </a>
+      <a href="/"
+         class="text-xs font-bold uppercase min-[600px]:pl-1">Resources</a>
+      <a href="/"
+         class="text-xs font-bold uppercase min-[600px]:pl-4">Archive</a>
+      <a href="/"
+         class="text-xs font-bold uppercase min-[600px]:pl-4">About</a>
    </div>
 
    <div class="hidden min-[600px]:flex min-[600px]:items-center min-[600px]:gap-x-4">
       <a href="/"
          class="text-xs font-bold uppercase">Account</a>
-         <a href="#"
-            class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full sm:flex-grow sm:w-auto">
-            Subscribe for Updates
-         </a>
+      <a href="#"
+         class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full sm:flex-grow sm:w-auto">
+         Subscribe for Updates
+      </a>
    </div>
 </nav>
