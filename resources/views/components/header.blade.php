6ab043b82321d01e6312862c0e733746a9f2ee14
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-03-20 16:46:35 -0400

    Fixed Tag CSS

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -14,6 +14,7 @@
 
     {{-- ALPINE DEMO --}}
 
+    {{--
     <div class="text-left relative xs:w-[138px] sm:w-min-28"
          x-data="{ show: false }">
         <div class="bg-gray-100 rounded-xl">
@@ -40,18 +41,53 @@
         </div>
         <div class="mt-1 py-2 bg-gray-100 absolute z-50 w-full rounded-xl border text-sm"
              x-show="show">
-             @foreach ($tags as $t)
+            @foreach ($tags as $t)
             <a href="/tag/{{ $t->url }}"
                class="{{ isset($tag) && $tag->is($t) ? 'bg-[#D8BFD8] text-white' : 'bg-gray-100' }}
-               px-3 block hover:bg-[#D8BFD8] hover:text-white focus:bg-[#D8BFD8] focus:text-white">
+        px-3 block hover:bg-[#D8BFD8] hover:text-white focus:bg-[#D8BFD8] focus:text-white">
                 {{ $t->name }}
             </a>
             @endforeach
         </div>
     </div>
+    --}}
 
     <div
          class="my-8 flex justify-center flex-wrap gap-x-4 gap-y-4 text-sm font-medium sm:mb-2 sm:flex-nowrap sm:justify-start">
+        <div class="flex-2 w-full text-left relative xs:flex-0 xs:max-w-fit sm:w-min-28"
+             x-data="{ show: false }">
+            <div class="bg-gray-100 rounded-xl">
+                <button @click="show = !show"
+                        @click.away="show = false"
+                        class="py-2 pl-3 pr-9 mr-9 inline-flex text-sm text-sm font-medium">
+                    {{ $tag->name ?? 'Tags' }}
+                    <svg class="transform -rotate-90 absolute pointer-events-auto"
+                         style="right: 12px"
+                         width="22"
+                         height="22"
+                         viewBox="0 0 22 22">
+                        <g fill="none"
+                           fill-rule="evenodd">
+                            <path stroke="#000"
+                                  stroke-opacity=".012"
+                                  stroke-width=".5"
+                                  d="M21 1v20.16H.84V1z"></path>
+                            <path fill="#222"
+                                  d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
+                        </g>
+                    </svg>
+                </button>
+            </div>
+            <div class="mt-1 py-2 bg-gray-100 absolute z-50 w-full rounded-xl border text-sm"
+                 x-show="show">
+                @foreach ($tags as $t)
+                <a href="/tag/{{ $t->url }}"
+                   class="{{ isset($tag) && $tag->is($t) ? 'bg-[#D8BFD8] text-white' : 'bg-gray-100' }} px-3 block hover:bg-[#D8BFD8] hover:text-white focus:bg-[#D8BFD8] focus:text-white">
+                    {{ $t->name }}
+                </a>
+                @endforeach
+            </div>
+        </div>
         <div class="min-w-28 relative flex flex-1 items-center bg-gray-100 rounded-xl sm:flex-none lg:inline-flex">
             <select class="py-2 pl-3 pr-9 flex-1 appearance-none bg-transparent">
                 <option value="category"
