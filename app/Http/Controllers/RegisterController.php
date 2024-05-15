e67129fca12b592ed50decd4de709edd59519781
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-05-15 11:40:29 -0400

    Pause

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -15,14 +15,19 @@ class RegisterController extends Controller
 
     function store()
     {
-        request()->validate([
+        extract(request()->all());
+        $url = str_replace(' ', '-', $name);
+
+        $attributes = request()->validate([
             'name' => 'required|max:125',
             'username' => 'required|min:7|max:125',
             'email' => 'required|email|max:125',
             'password' => 'required|min:7|max:20'
         ]);
 
-        User::create();
+        $attributes['url'] = strtolower($url);
+        
+        User::create($attributes);
 
         return view('registration.create');
 
