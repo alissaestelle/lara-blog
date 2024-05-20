ca856cda3f3835f5fa8df6f2a2228a33647118a3
Alissa Wiley <alissa.estelle.w@gmail.com>
2024-05-20 14:30:37 -0400

    Tolkien Good

--- resources/views/components/app/header.blade.php
+++ resources/views/components/app/header.blade.php
@@ -1,13 +1,15 @@
 @php
-$authorSearch = request('author') ?? false;
-$tagSearch = request('tag') ?? false;
+    $authorSearch = request('author') ?? false;
+    $tagSearch = request('tag') ?? false;
 @endphp
 
-<div class="mx-auto w-full flex flex-col gap-y-4 text-center base:px-5 base:py-6 base:text-left lg:mx-0">
+<div
+    class="mx-auto w-full flex flex-col gap-y-4 text-center base:px-5 base:py-6 base:text-left lg:mx-0">
     <div>
         <a href="/">
-            <h2 class="font-mono mt-3 text-3xl font-medium leading-6 text-gray-900 hover:text-gray-600 base:text-4xl"
-                style="font-family:'Courier New', Courier, monospace">
+            <h2
+                class="font-mono mt-3 text-3xl font-medium leading-6 text-gray-900 hover:text-gray-600 base:text-4xl"
+                style="font-family: 'Courier New', Courier, monospace">
                 Untitled #777
             </h2>
         </a>
@@ -17,29 +19,26 @@ $tagSearch = request('tag') ?? false;
     </div>
 
     <div
-         class="mx-auto my-8 w-full max-w-80 flex justify-center flex-wrap gap-x-4 gap-y-4 text-sm font-medium xs:mx-2.5 xs:max-w-none xs:w-auto sm:mx-0 base:mb-2 base:flex-nowrap base:justify-start">
-
+        class="mx-auto my-8 w-full max-w-80 flex justify-center flex-wrap gap-x-4 gap-y-4 text-sm font-medium xs:mx-2.5 xs:max-w-none xs:w-auto sm:mx-0 base:mb-2 base:flex-nowrap base:justify-start">
         <x-ui.tags />
 
         {{-- Search --}}
-        <div class="relative flex flex-1 items-center border rounded-xl base:flex-none lg:inline-flex">
-            <form method="GET"
-                  action="/search"
-                  class="mb-0">
+        <div
+            class="relative flex flex-1 items-center border rounded-xl base:flex-none lg:inline-flex">
+            <form method="GET" action="/search" class="mb-0">
                 @if ($authorSearch)
-                <input type="hidden"
-                       name="author"
-                       value="{{ $authorSearch }}">
+                    <input type="hidden" name="author" value="{{ $authorSearch }}" />
                 @endif
+
                 @if ($tagSearch)
-                <input type="hidden"
-                       name="tag"
-                       value="{{ $tagSearch }}">
+                    <input type="hidden" name="tag" value="{{ $tagSearch }}" />
                 @endif
-                <input type="text"
-                       name="keyword"
-                       placeholder="Search"
-                       class="px-3 py-2 text-gray-400 rounded-xl placeholder-gray-500" />
+
+                <input
+                    type="text"
+                    name="keyword"
+                    placeholder="Search"
+                    class="px-3 py-2 text-gray-400 rounded-xl placeholder-gray-500" />
             </form>
         </div>
     </div>
--- resources/views/components/app/nav.blade.php
+++ resources/views/components/app/nav.blade.php
@@ -1,85 +1,76 @@
 <nav
-     class="mx-auto pt-6 flex flex-wrap items-center justify-center gap-x-2 gap-y-2 base:justify-between xs:px-5 base:py-6 base:flex-nowrap base:gap-y-0 lg:">
+    class="mx-auto pt-6 flex flex-wrap items-center justify-center gap-x-2 gap-y-2 base:justify-between xs:px-5 base:py-6 base:flex-nowrap base:gap-y-0 lg:">
+    {{-- Left Nav for Mobile --}}
+    <a href="/" class="w-full base:hidden">
+        <img src="../images/howls.png" alt="Calcifer" class="mx-auto w-fit h-12" />
+    </a>
+    <a href="/" class="text-xs font-bold uppercase base:hidden">Resources</a>
+    <a href="/" class="text-xs font-bold uppercase base:hidden">Archive</a>
+    <a href="/" class="text-xs font-bold uppercase base:hidden">About</a>
 
-   {{-- Left Nav for Mobile --}}
-   <a href="/"
-      class="w-full base:hidden">
-      <img src="../images/howls.png"
-           alt="Calcifer"
-           class="mx-auto w-fit h-12" />
-   </a>
-   <a href="/"
-      class="text-xs font-bold uppercase base:hidden">Resources</a>
-   <a href="/"
-      class="text-xs font-bold uppercase base:hidden">Archive</a>
-   <a href="/"
-      class="text-xs font-bold uppercase base:hidden">About</a>
+    {{-- Right Nav for Mobile --}}
+    @auth
+        @php
+            $name = auth()->user()->name;
+            $user = explode(' ', $name)[0];
+        @endphp
 
-   {{-- Right Nav for Mobile --}}
-   @auth
-   @php
-   $name = auth()->user()->name;
-   $user = explode(" ", $name)[0];
-   @endphp
-   <div class="flex justify-center gap-x-2 w-full base:hidden">
-      <span class="text-xs font-bold uppercase text-[#D875C7]">Welcome, {{ $user }}</span>
-      <form method="POST"
-            action="/logout"
-            class="flex">
-         @csrf
-         <button type="submit"
-                 class="text-xs font-bold uppercase text-gray-400">Logout</button>
-      </form>
-   </div>
-   @else
-   <a href="/register"
-      class="text-xs font-bold uppercase base:hidden">Register</a>
-   <div class="mt-1 w-full text-center base:hidden">
-      <a href="#"
-         class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full">
-         Subscribe for Updates
-      </a>
-   </div>
-   @endauth
+        <div class="flex justify-center gap-x-2 w-full base:hidden">
+            <span class="text-xs font-bold uppercase text-[#D875C7]">Welcome, {{ $user }}</span>
+            <form method="POST" action="/logout" class="flex">
+                @csrf
+                <button type="submit" class="text-xs font-bold uppercase text-gray-400">
+                    Log Out
+                </button>
+            </form>
+        </div>
+    @else
+        <div class="flex justify-center gap-x-2 w-full text-xs font-bold uppercase base:hidden">
+            <a href="/register">Register</a>
+            <a href="/">Log In</a>
+        </div>
+        <div class="mt-1 w-full text-center base:hidden">
+            <a
+                href="#"
+                class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full">
+                Subscribe for Updates
+            </a>
+        </div>
+    @endauth
 
-   {{-- Left Nav for Desktop --}}
-   <div class="hidden base:flex base:items-center base:gap-x-4">
-      <a href="/"
-         class="shrink-0">
-         <img src="../images/howls.png"
-              alt="Calcifer"
-              class="w-fit h-10 base:mx-0" />
-      </a>
-      <a href="/"
-         class="text-xs font-bold uppercase">Resources</a>
-      <a href="/"
-         class="text-xs font-bold uppercase">Archive</a>
-      <a href="/"
-         class="text-xs font-bold uppercase">About</a>
-   </div>
+    {{-- Left Nav for Desktop --}}
+    <div class="hidden base:flex base:items-center base:gap-x-4">
+        <a href="/" class="shrink-0">
+            <img src="../images/howls.png" alt="Calcifer" class="w-fit h-10 base:mx-0" />
+        </a>
+        <a href="/" class="text-xs font-bold uppercase">Resources</a>
+        <a href="/" class="text-xs font-bold uppercase">Archive</a>
+        <a href="/" class="text-xs font-bold uppercase">About</a>
+    </div>
 
-   {{-- Right Nav for Desktop --}}
-   <div class="hidden base:flex base:items-center base:gap-x-4">
-      @auth
-      @php
-      $name = auth()->user()->name;
-      $user = explode(" ", $name)[0];
-      @endphp
-      <span class="text-xs font-bold uppercase text-[#D875C7]">Welcome, {{ $user }}</span>
-      <form method="POST"
-            action="/logout"
-            class="flex">
-         @csrf
-         <button type="submit"
-                 class="text-xs font-bold uppercase text-gray-400">Logout</button>
-      </form>
-      @else
-      <a href="/register"
-         class="text-xs font-bold uppercase">Register</a>
-      <a href="#"
-         class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full base:flex-grow base:w-auto">
-         Subscribe for Updates
-      </a>
-      @endauth
-   </div>
+    {{-- Right Nav for Desktop --}}
+    <div class="hidden base:flex base:items-center base:gap-x-4">
+        @auth
+            @php
+                $name = auth()->user()->name;
+                $user = explode(' ', $name)[0];
+            @endphp
+
+            <span class="text-xs font-bold uppercase text-[#D875C7]">Welcome, {{ $user }}</span>
+            <form method="POST" action="/logout" class="flex">
+                @csrf
+                <button type="submit" class="text-xs font-bold uppercase text-gray-400">
+                    Logout
+                </button>
+            </form>
+        @else
+            <a href="/register" class="text-xs font-bold uppercase">Register</a>
+            <a href="/" class="text-xs font-bold uppercase">Log In</a>
+            <a
+                href="#"
+                class="px-4 py-2 w-full bg-blue-500 text-xs font-semibold text-white uppercase rounded-full base:flex-grow base:w-auto">
+                Subscribe for Updates
+            </a>
+        @endauth
+    </div>
 </nav>
